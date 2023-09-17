f4d189754fed5fa0cb2795072f8aeb013136f075
KushKokate <142797968+KushKokate@users.noreply.github.com>
2023-09-17 16:52:27 +0530

    Add files via upload

---
+++ MyFrame.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+public class MyFrame {
+    public static void main(String args[]){
+        Frame frame  = new Frame("Kush @1234");
+        frame.resize(600, 600);
+        frame.setBackground(Color.red);
+
+        frame.show();
+    }
+}
